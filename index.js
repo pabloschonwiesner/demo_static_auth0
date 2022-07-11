cf82d6b48d8ffcaa20279637f7784076c275e737
Pablo Schonwiesner <pabloschonwiesner@gmail.com>
2022-07-11 10:03:37 -0300

    ADD certificates

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const https = require('https');
+const fs = require('fs')
+let history = require('connect-history-api-fallback');
+const path = require('path')
 
 const app = express()
 app.use(cors())
