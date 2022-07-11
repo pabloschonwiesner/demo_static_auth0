e9e7c22d88b6118ae7231c97fe9b745aba3b7de6
Pablo Schonwiesner <pabloschonwiesner@gmail.com>
2022-07-11 10:21:37 -0300

    ADD port for heroku

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const https = require('https');
 const fs = require('fs')
 let history = require('connect-history-api-fallback');
 const path = require('path')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: path.join(__dirname, '.env') })
 
 const app = express()
 app.use(cors())
@@ -14,6 +17,7 @@ const options = {
 };
 
 app.use(history({ index: '/' }))
+app.set('port', process.env.PORT)
 
 app.use('/public', express.static(path.join(__dirname, "/public/")));
 
@@ -21,4 +25,4 @@ app.get('/', function (req, res) {
   res.sendFile('index.html', { root: path.join(__dirname, '/public/') });
 });
 
-https.createServer(options, app).listen(3333);
+https.createServer(options, app).listen(process.env.PORT);
