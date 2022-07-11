a7333abf4a0b93a33026ece8d4355c066948f04d
Pablo Schonwiesner <pabloschonwiesner@gmail.com>
2022-07-11 10:44:02 -0300

    Replace https server to https heroku's server

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const https = require('https');
-const fs = require('fs')
 let history = require('connect-history-api-fallback');
 const path = require('path')
 const dotenv = require('dotenv')
@@ -10,11 +8,6 @@ dotenv.config({ path: path.join(__dirname, '.env') })
 
 const app = express()
 app.use(cors())
-
-const options = {
-  key: fs.readFileSync('localhost-key.pem'),
-  cert: fs.readFileSync('localhost.pem')
-};
 
 app.use(history({ index: '/' }))
 app.set('port', process.env.PORT)
@@ -25,4 +18,4 @@ app.get('/', function (req, res) {
   res.sendFile('index.html', { root: path.join(__dirname, '/public/') });
 });
 
-https.createServer(options, app).listen(process.env.PORT);
+app.listen(process.env.PORT);
