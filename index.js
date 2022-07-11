56a22d9c1579c65bdd93d60be7347bb60f2a16a6
Pablo Schonwiesner <pabloschonwiesner@gmail.com>
2022-07-11 09:38:10 -0300

    inmit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const https = require('https');
+
+const app = express()
+app.use(cors())
+
+const options = {
+  key: fs.readFileSync('localhost-key.pem'),
+  cert: fs.readFileSync('localhost.pem')
+};
+
+app.use(history({
+  index: '/'
+}))
+
+app.use('/public', express.static(path.join(__dirname, "/public/")));
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { root: path.join(__dirname, '/public/') });
+});
+
+https.createServer(options, app).listen(3333);
